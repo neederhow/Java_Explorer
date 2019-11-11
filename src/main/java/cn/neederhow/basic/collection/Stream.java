b4db3983ad8ae24f111e0e2a9df0793b63d7e636
nidh <nidh1@yonyou.com>
2019-11-11 18:18:04 +0800

    不安全方法

---
+++ src/main/java/cn/neederhow/basic/collection/Stream.java
@@ -0,0 +1,4 @@
+package cn.neederhow.basic.collection;
+
+public class Stream {
+}
---
+++ src/main/java/cn/neederhow/basic/timeHandle/SimpleDateFormatThreadSafe.java
@@ -0,0 +1,20 @@
+package cn.neederhow.basic.timeHandle;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class SimpleDateFormatThreadSafe {
+    static SimpleDateFormat sdf = new SimpleDateFormat("yyy-MM-dd HH:mm:ss");
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; ++i) {
+            Thread thread = new Thread(() -> {
+                try {//(3)使用单例日期实例解析文本
+                    System.out.println(sdf.parse("2017-12-13 15:17:27"));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            });
+            thread.start();//(4)启动线程
+        }
+    }
+}
