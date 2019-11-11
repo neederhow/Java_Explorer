a40be7fb350fd87add2f77b6be80b8257252e754
nidh <nidh1@yonyou.com>
2019-11-11 18:31:04 +0800

    配置maven编译级别

--- src/main/java/cn/neederhow/basic/timeHandle/SimpleDateFormatThreadSafe.java
+++ src/main/java/cn/neederhow/basic/timeHandle/SimpleDateFormatThreadSafe.java
@@ -4,12 +4,12 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class SimpleDateFormatThreadSafe {
-    static SimpleDateFormat sdf = new SimpleDateFormat("yyy-MM-dd HH:mm:ss");
+    static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     public static void main(String[] args) {
-        for (int i = 0; i <10 ; ++i) {
+        for (int i = 0; i <1000 ; ++i) {
             Thread thread = new Thread(() -> {
                 try {//(3)使用单例日期实例解析文本
-                    System.out.println(sdf.parse("2017-12-13 15:17:27"));
+                    System.out.println(sdf.parse("2019-12-13 15:17:27"));
                 } catch (ParseException e) {
                     e.printStackTrace();
                 }
